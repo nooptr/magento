342cdf490210298c259b7da719fb1d4916b911f5
Ngo Van Thang <thangnvbkhn@gmail.com>
2016-05-18 00:02:35 +0900

    退会機能

---
+++ module-customer/Controller/Account/Test.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ *
+ * Copyright © 2016 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Magento\Customer\Controller\Account;
+
+use Magento\Customer\Model\Session;
+use Magento\Framework\App\Action\Context;
+use Magento\Customer\Api\CustomerRepositoryInterface;
+use Magento\Framework\Registry;
+
+class Test extends \Magento\Customer\Controller\AbstractAccount
+{
+    /**
+     * @var Session
+     */
+    protected $session;
+    protected $customerRepository;
+
+    /**
+     * @param Context $context
+     * @param Session $customerSession
+     */
+    public function __construct(
+        Context $context,
+        Registry $registry,
+        Session $customerSession,
+        CustomerRepositoryInterface $customerRepository
+    ) {
+        $this->session = $customerSession;
+        $this->customerRepository = $customerRepository;
+        $registry->register('isSecureArea', true);
+        parent::__construct($context);
+    }
+
+    /**
+     * Customer logout action
+     *
+     * @return \Magento\Framework\Controller\Result\Redirect
+     */
+    public function execute()
+    {
+        $lastCustomerId = $this->session->getId();
+        //$this->session->logout()->setBeforeAuthUrl($this->_redirect->getRefererUrl())
+        //    ->setLastCustomerId($lastCustomerId);
+        $this->session->logout();
+        $this->customerRepository->deleteById($lastCustomerId);
+
+        /** @var \Magento\Framework\Controller\Result\Redirect $resultRedirect */
+        $resultRedirect = $this->resultRedirectFactory->create();
+        return $resultRedirect->setPath('*/*/logoutSuccess');
+        return $resultRedirect;
+    }
+}
